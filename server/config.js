fedaaddaf16d3e19c5e2599a57aefac91b523d3e
Tim Boyce <timothy.boyce@mmgins.com>
2017-04-18 11:46:20 -0400

    support SSL

--- server/config.js
+++ server/config.js
@@ -1,4 +1,10 @@
 module.exports = {
   builds_base_url: 'http://builds/',
-  deployments_base_url: 'http://deployments/'
+  deployments_base_url: 'http://deployments/',
+  ssl: {
+    active: true,
+    key: 'server.key',
+    certificate: 'server.crt'
+  },
+  port: 3000
 };
--- server/server.js
+++ server/server.js
@@ -6,14 +6,18 @@ const deployments = require('./routes/deployments');
 const maintenance = require('./routes/maintenance');
 const status = require('./routes/status');
 
+var fs = require('fs');
+var https = require('https');
+var config = require('./config');
+
 const app = express();
 
 // CORS
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
-    next();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+  next();
 });
 
 app.use('/', express.static('../client/dist'));
@@ -26,16 +30,35 @@ app.use('/api/v1/maintenance', maintenance);
 app.use('/api/v1/status', status);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-let server = app.listen(3000, function() {
-    const host = 'localhost';
-    const port = server.address().port;
-    console.log('App listening at http://%s:%s', host, port);
+function setup(ssl) {
+  if (ssl && ssl.active) {
+    return {
+      key: fs.readFileSync(ssl.key),
+      cert: fs.readFileSync(ssl.certificate)
+    };
+  }
+}
+
+function start(app, options) {
+  if (options)
+    return require('https').createServer(options, app);
+
+  return require('http').createServer(app);
+}
+
+var options = setup(config.ssl);
+
+var server = start(app, options).listen(config.port, function () {
+  const host = 'localhost';
+  const port = server.address().port;
+  const scheme = config.ssl.active ? 'https' : 'http';
+  console.log('App listening at %s://%s:%s', scheme, host, port);
 });
 
 module.exports = app;
