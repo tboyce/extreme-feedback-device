22151d1844cd5ed20a57d5617dbfb0466b5c30a0
Timothy Boyce <timothy.boyce@mmgins.com>
2017-05-17 20:22:41 -0400

    update angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,21 @@
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-import { Ng2PaginationModule } from 'ng2-pagination';
-import { MomentModule } from 'angular2-moment';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {
+  MdButtonModule, MdChipsModule, MdIconModule, MdListModule, MdSlideToggleModule,
+  MdTabsModule, MdToolbarModule, MdTooltipModule
+} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from '@angular/router';
+import {NgxPaginationModule} from 'ngx-pagination';
+import {MomentModule} from 'angular2-moment';
 
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ManageMaintenanceComponent } from './manage/manage-maintenance/manage-maintenance.component';
-import { ManageStatusItemsComponent } from './manage/manage-status-items/manage-status-items.component';
-import { ManageComponent } from './manage/manage.component';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {ManageMaintenanceComponent} from './manage/manage-maintenance/manage-maintenance.component';
+import {ManageStatusItemsComponent} from './manage/manage-status-items/manage-status-items.component';
+import {ManageComponent} from './manage/manage.component';
 
 const appRoutes: Routes = [
   {path: '', component: DashboardComponent},
@@ -28,10 +32,18 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     ReactiveFormsModule,
     HttpModule,
-    MaterialModule,
-    Ng2PaginationModule,
+    NgxPaginationModule,
+    MdButtonModule,
+    MdChipsModule,
+    MdIconModule,
+    MdListModule,
+    MdSlideToggleModule,
+    MdTabsModule,
+    MdToolbarModule,
+    MdTooltipModule,
     MomentModule,
     RouterModule.forRoot(appRoutes, {useHash: true})
   ],
